8ce72dbfd18a69958c4135c37002bb6c3ea7e855
Elaa Hmida <hmidaelaa9@gmail.com>
2023-01-26 20:33:24 +0100

    first commit add active user in last 30days

--- src/views/main/Dashboard.vue
+++ src/views/main/Dashboard.vue
@@ -1,37 +1,76 @@
 <template>
   <v-container fluid>
-    <v-card class="ma-3 pa-3">
-      <v-card-title primary-title>
-        <div class="headline primary--text">Dashboard</div>
-      </v-card-title>
-      <v-card-text>
-        <div class="headline font-weight-light ma-5">Welcome {{ greetedUser }}</div>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn to="/main/profile/view">View Profile</v-btn>
-        <v-btn to="/main/profile/edit">Edit Profile</v-btn>
-        <v-btn to="/main/profile/password">Change Password</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-card class="ma-3 pa-3"> </v-card>
+    <br />
+    <div class="row">
+      <canvas id="myChart"></canvas>
+    </div>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { readUserProfile } from "@/store/main/getters";
+import Chart from "chart.js/auto";
 
-@Component
-export default class Dashboard extends Vue {
-  get greetedUser() {
-    const userProfile = readUserProfile(this.$store);
-    if (userProfile) {
-      if (userProfile.full_name) {
-        return userProfile.full_name;
-      } else {
-        return userProfile.email;
-      }
-    }
-    return "";
-  }
-}
+export default {
+  name: "StatisticsAchba7",
+  props: {
+    msg: String,
+  },
+  mounted() {
+    const ctx = document.getElementById("myChart");
+    const labels = [
+      "2022-12-27",
+      "2022-12-28",
+      "2022-12-29",
+      "2022-12-30",
+      "2022-12-31",
+      "2023-01-01",
+      "2023-01-02",
+      "2023-01-03",
+      "2023-01-04",
+      "2023-01-05",
+      "2023-01-06",
+      "2023-01-07",
+      "2023-01-08",
+      "2023-01-09",
+      "2023-01-10",
+      "2023-01-11",
+      "2023-01-12",
+      "2023-01-13",
+      "2023-01-14",
+      "2023-01-15",
+      "2023-01-16",
+      "2023-01-17",
+      "2023-01-18",
+      "2023-01-19",
+      "2023-01-20",
+      "2023-01-21",
+      "2023-01-22",
+      "2023-01-23",
+      "2023-01-24",
+      "2023-01-25",
+      "2023-01-26",
+    ];
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Active Users in the last 30 days",
+          data: [
+            874, 635, 135, 732, 317, 397, 766, 424, 848, 666, 82, 1, 608, 196, 715, 342,
+            163, 245, 228, 652, 458, 387, 727, 896, 689, 581, 895, 424, 32, 411, 892,
+          ],
+          fill: false,
+          borderColor: "rgb(255, 99, 132)",
+          tension: 0.1,
+        },
+      ],
+    };
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: data,
+    });
+    myChart;
+  },
+};
 </script>
